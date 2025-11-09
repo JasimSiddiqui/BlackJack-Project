5bb89c4667239f3d2cc4000d2b8f1739becce92d
Jasim Siddiqui <jasims.0002@gmail.com>
2025-11-09 14:16:39 -0500

    Cash

--- code.py
+++ code.py
@@ -2,6 +2,8 @@
 
 - 2025/11/09 12:54 am: Starting the project. Will try to get all the code for the user functions done.
 - 2025/11/09 01:54 am: Completed basic code for blackjack game to play.  
+- 2025/11/09 02:07 pm: Added ability to play multiple games.
+- 2025/11/09 02:15 pm: Added money/betting gameplay
 
 '''
 import random, os
@@ -13,6 +15,7 @@ wins = 0
 losses = 0
 ties = 0
 games = 0
+cash = 1000
 cards = ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King']
 suits = [' of Hearts', ' of Clubs', ' of Diamonds', ' of Spades']
 
@@ -61,6 +64,16 @@ while True:
     player_hand = []
     dealer_hand = []
     #Card dealing (start with 2 cards each)
+    while True:
+        try:
+            bet = int(input(f'How much money would you like to bet? Current Balance: ${cash}\n> '))
+            if bet <= cash:
+                cash -= bet
+                break
+            else:
+                print('Max bet is total cash')
+        except:
+            print('Please enter an integer value!')
     dealer_hand.append(pick_card())
     player_hand.append(pick_card())
     dealer_hand.append(pick_card())
@@ -84,6 +97,8 @@ while True:
         if count_total(dealer_hand) < 16:
             dealer_hand.append(pick_card())
             print('Dealer hits.')
+        else:
+            print('Dealer stands.')
         #If the dealer busts, break
         if count_total(dealer_hand) > 21:
             break
@@ -101,9 +116,11 @@ while True:
     elif count_total(dealer_hand) > 21:
         print('Dealer busted! You win!')
         wins += 1
+        cash += bet * 2
     elif count_total(player_hand) == 21 and count_total(dealer_hand) != 21:
         print('BLACKJACK! You win!')
         wins += 1
+        cash += bet * 2
     elif count_total(player_hand) != 21 and count_total(dealer_hand) == 21:
         print('BLACKJACK! You lose!')
         losses += 1
@@ -113,13 +130,22 @@ while True:
     elif count_total(dealer_hand) < count_total(player_hand):
         print('Your total is higher. You win!')
         wins += 1
+        cash += bet * 2
     else:
         print('Tie!')
         ties += 1
+        cash += bet
+
+    #Can't play again if they go broke
+    if cash <= 0:
+        print('You lost all your money!')
+        break
+
     #Can choose to play again (go back to start of first While loop)
     play_again = input('Would you like to play again?(y/n)\n> ')
     if not play_again.lower().startswith('y'):
         break
     else:
         os.system('cls')
-print(f'Out of {games} games, you won {wins}, lost {losses} and tied {ties}')
+
+print(f'Out of {games} games, you won {wins}, lost {losses} and tied {ties}.\nYou leave with {cash} dollars.')
