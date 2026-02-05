7094da8685a49440f24705cec43f36da1975ad42
Jasim S. <158212695+JasimSiddiqui@users.noreply.github.com>
2026-02-05 04:39:23 -0500

    removed time log in headder of code

--- code.py
+++ code.py
@@ -1,9 +1,6 @@
 '''
 
-- 2025/11/09 12:54 am: Starting the project. Will try to get all the code for the user functions done.
-- 2025/11/09 01:54 am: Completed basic code for blackjack game to play.  
-- 2025/11/09 02:07 pm: Added ability to play multiple games.
-- 2025/11/09 02:15 pm: Added money/betting gameplay
+
 
 '''
 import random, os
